832e9bb6b76b6c9125f766a11b1712b5401b0286
Lee Martin <lbm13@uab.edu>
2021-10-06 13:53:10 -0500

    initial for tsp sa

---
+++ tsp/setup.py
@@ -0,0 +1,66 @@
+from typing import Final
+from random import randint
+from math import sqrt
+import queue
+
+NUM_NODES: Final = 100
+MAX_X: Final = 100
+MAX_Y: Final = 100
+
+def generateNodes():
+    nodes = set()
+    for i in range(NUM_NODES):
+        x = randint(0, MAX_X)
+        y = randint(0, MAX_Y)
+        nodes.add((f"node{i}", (x, y)))
+    return list(nodes)
+
+def generateEdges(nodes):
+    edges = set()
+    for start in nodes:
+        for end in nodes:
+            if (end, start) not in edges: edges.add((start, end))
+
+    return list(edges)
+
+def generateGraph(edges):
+    graph = {}
+    for edge in edges:
+        node1 = edge[0]
+        node2 = edge[1]
+        dx2 = (node1[1][0] - node2[1][0])**2
+        dy2 = (node1[1][1] - node2[1][1])**2
+        distance = int(sqrt(dx2 + dy2))
+        node1Name = node1[0]
+        node2Name = node2[0]
+        if node1Name not in graph: 
+            graph[node1Name] = {}
+            graph[node1Name][node2Name] = distance
+        else: graph[node1Name][node2Name] = distance
+        if node2Name not in graph: 
+            graph[node2Name] = {}
+            graph[node2Name][node1Name] = distance
+        else: graph[node2Name][node1Name] = distance
+    return graph
+
+def randomTour(graph):
+    tour = []
+    graph = dict(graph)
+    nodes = list(graph.keys())
+    while len(nodes) > 0:
+        tour.append(nodes.pop(randint(0, len(nodes) - 1)))
+    return tour
+
+def evaluateTour(tour, graph):
+    totalDistance = 0
+    for i in range(len(tour) - 2):
+        current = tour[i]
+        next = tour[i + 1]
+        totalDistance = totalDistance + graph[current][next]
+    return totalDistance
+
+# nodes = generateNodes()
+# edges = generateEdges(nodes)
+# graph = generateGraph(edges)
+# tour = randomTour(graph)
+# evaluateTour(tour, graph)
---
+++ tsp/tsp_sa.py
@@ -0,0 +1,68 @@
+import setup
+from random import randint, random
+from math import exp
+
+class tsp():
+    nodes = []
+    edges = []
+    distances = {}
+
+    def __init__(self):
+        self.nodes = setup.generateNodes()
+        self.edges = setup.generateEdges(self.nodes)
+        self.graph = setup.generateGraph(self.edges)
+        self.initialTour = setup.randomTour(self.graph)
+        self.initialTemperature = 100000
+
+    def sa(self):
+        current = self.initialTour
+        currentEvaluation = setup.evaluateTour(current, self.graph)
+        temperature = self.initialTemperature
+        while temperature > 0:
+            candidate = self.swap(current)
+            candidateEvaluation = setup.evaluateTour(candidate, self.graph)
+            dE = currentEvaluation - candidateEvaluation
+
+            # we improve the situation
+            if (dE > 0): 
+                current = candidate
+                currentEvaluation = candidateEvaluation
+            
+            #or we don't
+            else:
+                probability = self.giveMeProbability(dE, temperature)
+                randomNum = random()
+                if randomNum < probability:
+                    current = candidate
+                    currentEvaluation = candidateEvaluation
+                temperature = self.schedule(temperature)
+        print(f"best route evaluation is: {currentEvaluation}\n")
+        return current
+            
+    def giveMeProbability(self, dE, temperature):
+        return exp((dE) / temperature)
+
+    def schedule(self, temperature):
+        return temperature - 1
+
+    def swap(self, tour):
+        randomI1 = 0
+        randomI2 = 0
+        while randomI1 == randomI2:
+            randomI1 = randint(0, len(tour) - 1)
+            randomI2 = randint(0, len(tour) - 1)
+        temp = tour[randomI1]
+        tour[randomI1] = tour[randomI2]
+        tour[randomI2] = temp
+        return tour
+
+tsp = tsp()
+print(tsp.sa())
+
+    
+
+
+    
+
+# tsp = tsp()
+# setup.generateNeighbors(tsp.nodes[0])
